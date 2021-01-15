28031fb748d8d41acb7e9212d1c67f9691a3aa74
jirayu123za <76035470+jirayu123za@users.noreply.github.com>
2021-01-15 15:52:12 +0700

    finish1_0

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int x);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int x){
+	int sum = 0;
+	if(x == 0){
+		return sum;
+	}else if(x == 1){
+		sum = 1;
+		return sum;
+	}else{
+		sum = sum+fibonacci(x-1) + fibonacci(x-2);
+		return sum;
+	}
 }
